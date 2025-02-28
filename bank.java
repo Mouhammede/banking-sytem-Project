b52bb82376d6503aa4e9686515bcae143bdfa801
mouhamed <righi.mohammedtahar@gmail.com>
2025-02-28 23:04:44 +0100

    update 1

--- bank.java
+++ bank.java
@@ -2,4 +2,11 @@ public class bank
 {
     public String name;
     public String location;
+    public class account
+    {
+        private int password;
+        private int key;
+        private double balence;
+        private int ID;
+    }
 }
